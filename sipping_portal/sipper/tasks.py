919cf3da2a189edf53d14b9e1cb48a14c88bdfb0
Forest Dussault <forest.dussault@inspection.gc.ca>
2017-11-24 15:39:01 -0500

    Functionality submitting jobs

--- sipping_portal/sipper/tasks.py
+++ sipping_portal/sipper/tasks.py
@@ -1,6 +1,8 @@
 import glob
 import os
 
+from django.core import serializers
+
 from subprocess import Popen, PIPE
 from background_task import background
 
@@ -9,11 +11,42 @@ def miseq_directory_list(cmd):
     """
     Pass in a command line command to the genesippr Docker container
     """
-    cmd = 'docker exec ' \
-          'sipping_portal_genesipprv2 ' \
-          '{}'.format(cmd)
-
+    cmd = 'docker exec sippingportal_genesipprv2 {}'.format(cmd)
     p = Popen(cmd, shell=True, stdout=PIPE)
     output = p.communicate()[0].splitlines()  # wait until the script completes before resuming the code
-
     return output
+
+
+def execute_command(cmd):
+    p = Popen(cmd, shell=True)
+    p.communicate()
+
+
+@background(schedule=1)
+def start_sipping(target_folder, json_model):
+    print('\nSuccessfully called start_sipping method on {}'.format(target_folder))
+
+    # Deserialize JSON model
+    run_model = serializers.deserialize('json', json_model)
+
+    # Make output folder
+    cmd = 'docker exec sippingportal_genesipprv2 ' \
+          'mkdir miseq_output/{}'.format(target_folder)
+    execute_command(cmd)
+
+    # Run genesippr
+    cmd = 'docker exec sippingportal_genesipprv2 ' \
+          'method.py miseq_output/{target_folder} ' \
+          '-t /targets ' \
+          '-m /miseq ' \
+          '-f {target_folder} ' \
+          '-b -C -r2 0'.format(target_folder=target_folder)
+    execute_command(cmd)
+
+    # Update model status
+    for obj in run_model:
+        obj.object.genesippr_status = 'Complete'
+        obj.save()
+        print('Updated genesippr_status to {}'.format(obj.object.genesippr_status))
+
+    print('Genesippr job for {} complete'.format(target_folder))
