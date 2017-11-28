cb414e561a347b3abafe35e681efb8f88b83c0e4
Forest Dussault <forest.dussault@inspection.gc.ca>
2017-11-28 11:44:36 -0500

    Added live log reading

--- sipping_portal/sipper/tasks.py
+++ sipping_portal/sipper/tasks.py
@@ -4,8 +4,10 @@ import os
 from django.core import serializers
 
 from subprocess import Popen, PIPE
+from time import sleep
 from background_task import background
 
+from .models import SipperRun
 
 def miseq_directory_list(cmd):
     """
@@ -22,31 +24,90 @@ def execute_command(cmd):
     p.communicate()
 
 
-@background(schedule=1)
-def start_sipping(target_folder, json_model):
-    print('\nSuccessfully called start_sipping method on {}'.format(target_folder))
+@background(schedule=10)
+def active_log_reader(json_model, json_metadata_model):
+    print('\nCALLED active_log_reader BACKGROUND TASK')
 
-    # Deserialize JSON model
+    # Deserialize models
+    run_metadata_model = serializers.deserialize('json', json_metadata_model)
+    run_model = serializers.deserialize('json', json_model)
+
+    for obj in run_model:
+            for meta_obj in run_metadata_model:
+                while obj.object.genesippr_status == 'Processing':
+
+                    #print('\nAttempting to pull recent log data from {}'.format(meta_obj.object.log_filepath))
+                    recent_output = read_log_recent_line(meta_obj.object.log_filepath)
+
+                    if recent_output is not None:
+                        meta_obj.object.recent_output = recent_output
+                        meta_obj.save(force_update=True)
+                        print('\nSUCCESSFULLY UPDATED RECENT OUTPUT')
+
+                        #print('Updated recent output to {}'.format(meta_obj.object.recent_output))
+                    else:
+                        print('\nCANNOT DETECT OUTPUT. WAITING.')
+
+                    sleep(30) # Update model every 30 seconds
+
+
+@background(schedule=1)
+def run_genesippr(target_folder, json_model):
+    print('\nCALLED run_genesippr BACKGROUND TASK')
+
+    # Deserialize
     run_model = serializers.deserialize('json', json_model)
 
     # Make output folder
-    cmd = 'docker exec sippingportal_genesipprv2 ' \
-          'mkdir miseq_output/{}'.format(target_folder)
-    execute_command(cmd)
+    print('\nCreating output folder for genesippr...')
+    mkdir_cmd = 'docker exec sippingportal_genesipprv2 ' \
+                'mkdir miseq_output/{}'.format(target_folder)
+    execute_command(mkdir_cmd)
 
+    print('\nRunning genesippr...')
     # Run genesippr
-    cmd = 'docker exec sippingportal_genesipprv2 ' \
-          'method.py miseq_output/{target_folder} ' \
-          '-t /targets ' \
-          '-m /miseq ' \
-          '-f {target_folder} ' \
-          '-b -C -r2 0'.format(target_folder=target_folder)
-    execute_command(cmd)
+    genesippr_cmd = 'docker exec sippingportal_genesipprv2 ' \
+                    'method.py miseq_output/{target_folder} ' \
+                    '-t /targets ' \
+                    '-m /miseq ' \
+                    '-f {target_folder} ' \
+                    '-b -C -r2 0'.format(target_folder=target_folder)
+    execute_command(genesippr_cmd)
 
     # Update model status
     for obj in run_model:
         obj.object.genesippr_status = 'Complete'
-        obj.save()
+        obj.save(force_update=True)
         print('Updated genesippr_status to {}'.format(obj.object.genesippr_status))
 
     print('Genesippr job for {} complete'.format(target_folder))
+
+
+def start_sipping(target_folder, json_model, json_metadata_model):
+    print('\nSuccessfully called start_sipping method on {}'.format(target_folder))
+
+    # Call sippr background task
+    run_genesippr(target_folder, json_model)
+
+    # Start log reader
+    active_log_reader(json_model, json_metadata_model)
+
+
+def read_log_recent_line(log_filepath):
+    current_time = str()
+
+    try:
+        log_file = open(log_filepath, 'r')
+        log_lines = log_file.readlines()
+        log_file.close()
+        current_line = log_lines[len(log_lines)-1]
+        return current_line
+    except FileNotFoundError:
+        print('\nCANNOT FIND FILE {}'.format(log_filepath))
+
+
+def read_log_metadata(log_filepath):
+    miseq_path = str()
+    miseq_folder = str()
+    fastq_destination = str()
+    samplesheet = str()
--- sipping_portal/sipper/views.py
+++ sipping_portal/sipper/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.views.generic import TemplateView, FormView
+from django.views.generic import TemplateView, FormView, DetailView
 from django_tables2 import RequestConfig
 from django.core import serializers
 from .tasks import miseq_directory_list, start_sipping
@@ -20,18 +20,15 @@ class FileSystemViewer(TemplateView):
     form_class = RunForm
 
     def get(self, request, *args, **kwargs):
-        out = miseq_directory_list('ls miseq/')
 
-        # Debug
-        for miseq_run in out:
-            run_object = SipperRun.objects.get_or_create(target_folder=miseq_run)
-            print('Run Object: {}'.format(run_object))
-
+        # Get all run objects
         miseq_runs = SipperRun.objects.all()
 
+        # Table setup
         table = SipperTable(miseq_runs)
         RequestConfig(request).configure(table)
 
+        # Context
         context = {
             'miseq_runs': miseq_runs,
             'table': table,
@@ -45,23 +42,52 @@ class FileSystemViewer(TemplateView):
         target_folder = request.POST.get('miseq_run')
         print('Target folder: {}\n'.format(target_folder))
 
+        # Grab MiSeq run selected by user
         run_model = SipperRun.objects.get(target_folder=target_folder)
 
+        # Create object in DB for metadata storage
         SippingMetadata.objects.create(run=run_model)
         print('Created SippingMetadata object...\n')
 
+        # Get created object
+        run_metadata_model = SippingMetadata.objects.get(run=run_model)
+
+        # Update Genesippr status
         run_model.genesippr_status = 'Processing'
         run_model.save()
         print('Attempted to set genesippr_status to Processing\n'
                       'Actual value is: {}'.format(run_model.genesippr_status))
 
-        json_model = serializers.serialize('json', [run_model])
-        start_sipping(target_folder, json_model)
+        # Set log file location
+        run_metadata_model.log_filepath = 'sipping_portal/media/{target_folder}/portal.log'.format(target_folder=target_folder)
+        run_metadata_model.save()
 
+        # Serialize model instance so it can be passed to sipping function
+        json_model = serializers.serialize('json', [run_model])
+        json_metadata_model = serializers.serialize('json', [run_metadata_model])
+        start_sipping(target_folder, json_model, json_metadata_model)
+
+        # Update page
         return HttpResponseRedirect('#')
 
+def active_run(request):
+    # Get current run (if any)
+    currently_processing_run = SipperRun.objects.filter(genesippr_status='Processing')
+    if len(currently_processing_run) > 1:
+        print('Something is wrong. Detected >1 "Processing" GenesipprV2 run.')
 
+    try:
+        currently_processing_run = SipperRun.objects.filter(genesippr_status='Processing')[0]
+    except IndexError:
+        currently_processing_run = None
 
+    current_run_metadata = SippingMetadata.objects.get(run=currently_processing_run.pk)
 
+    context = {
+        'currently_processing_run' : currently_processing_run,
+        'current_run_metadata' : current_run_metadata,
+    }
 
-
+    return render(request,
+                  'sipper/active_run.html',
+                  context)
