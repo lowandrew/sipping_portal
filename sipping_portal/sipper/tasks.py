68225f70302f6d6a381d831f0991ec26bc98b00b
Forest Dussault <forest.dussault@inspection.gc.ca>
2017-11-28 14:18:14 -0500

    Added metadata model updating functionality

--- sipping_portal/sipper/tasks.py
+++ sipping_portal/sipper/tasks.py
@@ -7,7 +7,6 @@ from subprocess import Popen, PIPE
 from time import sleep
 from background_task import background
 
-from .models import SipperRun
 
 def miseq_directory_list(cmd):
     """
@@ -31,20 +30,60 @@ def active_log_reader(json_model, json_metadata_model):
     # Deserialize models
     run_metadata_model = serializers.deserialize('json', json_metadata_model)
     run_model = serializers.deserialize('json', json_model)
+    read_log_metadata = True
 
     for obj in run_model:
             for meta_obj in run_metadata_model:
                 while obj.object.genesippr_status == 'Processing':
+                    raw_lines = read_logfile(meta_obj.object.log_filepath)
 
-                    #print('\nAttempting to pull recent log data from {}'.format(meta_obj.object.log_filepath))
-                    recent_output = read_log_recent_line(meta_obj.object.log_filepath)
+                    # Read if the log can be found
+                    if raw_lines is not None:
 
-                    if recent_output is not None:
-                        meta_obj.object.recent_output = recent_output
+                        # Parse the actual output lines
+                        recent_output = remove_extraneous_log_metadata(raw_lines)
+
+                        # Read the log once for file locations
+                        if read_log_metadata:
+                            miseq_path, miseq_folder, fastq_destination, samplesheet = pull_log_metadata(raw_lines)
+                            meta_obj.object.miseq_path = miseq_path
+                            meta_obj.object.miseq_folder = miseq_folder
+                            meta_obj.object.fastq_destination = fastq_destination
+                            meta_obj.object.samplesheet = samplesheet
+                            meta_obj.save(force_update=True)
+
+                            # Skip this next time around
+                            read_log_metadata = False
+
+                        # Rather unfortunate looking model update
+                        try:
+                            meta_obj.object.recent_output_1_time, \
+                            meta_obj.object.recent_output_1 = time_content_log_split(recent_output[0])
+                        except IndexError:
+                            pass
+                        try:
+                            meta_obj.object.recent_output_2_time, \
+                            meta_obj.object.recent_output_2 = time_content_log_split(recent_output[1])
+                        except IndexError:
+                            pass
+                        try:
+                            meta_obj.object.recent_output_3_time, \
+                            meta_obj.object.recent_output_3 = time_content_log_split(recent_output[2])
+                        except IndexError:
+                            pass
+                        try:
+                            meta_obj.object.recent_output_4_time, \
+                            meta_obj.object.recent_output_4 = time_content_log_split(recent_output[3])
+                        except IndexError:
+                            pass
+                        try:
+                            meta_obj.object.recent_output_5_time, \
+                            meta_obj.object.recent_output_5 = time_content_log_split(recent_output[4])
+                        except IndexError:
+                            pass
+
+                        # Save
                         meta_obj.save(force_update=True)
-                        print('\nSUCCESSFULLY UPDATED RECENT OUTPUT')
-
-                        #print('Updated recent output to {}'.format(meta_obj.object.recent_output))
                     else:
                         print('\nCANNOT DETECT OUTPUT. WAITING.')
 
@@ -93,21 +132,58 @@ def start_sipping(target_folder, json_model, json_metadata_model):
     active_log_reader(json_model, json_metadata_model)
 
 
-def read_log_recent_line(log_filepath):
-    current_time = str()
-
+def read_logfile(log_filepath):
     try:
         log_file = open(log_filepath, 'r')
         log_lines = log_file.readlines()
         log_file.close()
-        current_line = log_lines[len(log_lines)-1]
-        return current_line
+        return log_lines
     except FileNotFoundError:
         print('\nCANNOT FIND FILE {}'.format(log_filepath))
 
 
-def read_log_metadata(log_filepath):
+def pull_log_metadata(line_list):
     miseq_path = str()
     miseq_folder = str()
     fastq_destination = str()
     samplesheet = str()
+
+    for item in line_list[:10]:
+        if item.startswith('MiSeqPath:'):
+            miseq_path = item.replace(',', '').replace('MiSeqPath: ', '')
+        elif item.startswith('MiSeqFolder'):
+            miseq_folder = item.replace(',', '').replace('MiSeqFolder: ', '')
+        elif item.startswith('Fastq destination'):
+            fastq_destination = item.replace(',', '').replace('Fastq destination: ', '')
+        elif item.startswith('SampleSheet'):
+            samplesheet = item.replace(',', '').replace('SampleSheet: ', '')
+    return miseq_path, miseq_folder, fastq_destination, samplesheet
+
+
+def remove_extraneous_log_metadata(line_list):
+    cleaned_list = []
+
+    for item in line_list:
+        if item.startswith('MiSeqPath:'):
+            miseq_path = item.replace(',', '').replace('MiSeqPath: ', '')
+        elif item.startswith('MiSeqFolder'):
+            miseq_folder = item.replace(',', '').replace('MiSeqFolder: ', '')
+        elif item.startswith('Fastq destination'):
+            fastq_destination = item.replace(',', '').replace('Fastq destination: ', '')
+        elif item.startswith('SampleSheet'):
+            samplesheet = item.replace(',', '').replace('SampleSheet: ', '')
+        else:
+            cleaned_list.append(item)
+        # Take last 5 items
+        cleaned_list = cleaned_list[-5:]
+    return cleaned_list
+
+
+def time_content_log_split(log_line):
+    tmp = log_line.replace('[', '')
+    tmp = tmp.split('] ')
+
+    time = tmp[0]
+    content = tmp[1]
+
+    return time, content
